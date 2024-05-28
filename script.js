50bbae20925370482bbead85c0b3f3c161c8139a
JonasTheOtter <darrylmanga@yahoo.com>
2024-05-28 17:12:17 +0200

    added Favicon and some logic

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-async function fetchDataJSON(){
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=15&offset=0");
-    let resopnseAsJSON = await response.json(); 
-    
+let Pokedex = url="https://pokeapi.co/api/v2/pokemon?limit=24&offset=809";
 
-console.log(resopnseAsJSON);
+async function fetchDataJSON(){
+  
+    let response = await fetch(Pokedex);
+    let resopnseAsJSON = await response.json(); 
 for (let i=0; i<resopnseAsJSON.results.length; i++){
     let Pokemons = await resopnseAsJSON.results[i]['url'];
     let Pokemon = await fetch(Pokemons);
@@ -11,24 +11,37 @@ for (let i=0; i<resopnseAsJSON.results.length; i++){
     let Front = await fetch (PokemonAsJSON['sprites']['other']['official-artwork']['front_default']);
     let Front_img = Front['url'];
 
-    document.getElementById('content').innerHTML += `<div id ="smallCard${i}" class="smallCard">
-    <div class="PokeImage">
-    <img class="pokemon-img" src="${Front_img}">
-    </div>
+        document.getElementById('content').innerHTML += `<div id ="smallCard${i}" class="smallCard">
+               <div class="PokeImage">
+                <img class="pokemon-img" src="${Front_img}">
+               </div>
     <div class="PokeInfo">
      Name: ${resopnseAsJSON.results[i]['name']} <br>
      Number: ${PokemonAsJSON['id']} <br>
      ${PokemonAsJSON['types'][0]['type']['name']}
      </div>
     </div>`;
+//     }else{
+//         document.getElementById('content').innerHTML += `<div id ="smallCard${i}" class="smallCard">
+//         <div class="PokeImage">
+//          <img class="pokemon-img" src="${Front_img}">
+//         </div>
+// <div class="PokeInfo">
+// Name: ${resopnseAsJSON.results[i]['name']} <br>
+// Number: ${PokemonAsJSON['id']} <br>
+// ${PokemonAsJSON['types'][0]['type']['name']}
+// ${PokemonAsJSON['types'][1]['type']['name']}
+// </div>
+// </div>`;
+//     };
+  
 };
 giveTypeColor();
 }
 
 async function giveTypeColor(){
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=15&offset=0");
+    let response = await fetch(Pokedex);
     let resopnseAsJSON = await response.json(); 
-    
 
     for (let i=0; i<resopnseAsJSON.results.length; i++){
         let Pokemons = await resopnseAsJSON.results[i]['url'];

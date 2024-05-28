9aa23e28787736150a5e3ec1871b8e38520aac84
JonasTheOtter <darrylmanga@yahoo.com>
2024-05-28 15:40:03 +0200

    adding color to types

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 async function fetchDataJSON(){
     let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=15&offset=0");
-    let resopnseAsJSON = await response.json();
+    let resopnseAsJSON = await response.json(); 
+    
 
 console.log(resopnseAsJSON);
 for (let i=0; i<resopnseAsJSON.results.length; i++){
@@ -10,7 +11,7 @@ for (let i=0; i<resopnseAsJSON.results.length; i++){
     let Front = await fetch (PokemonAsJSON['sprites']['other']['official-artwork']['front_default']);
     let Front_img = Front['url'];
 
-    document.getElementById('content').innerHTML += `<div id ="smallCard">
+    document.getElementById('content').innerHTML += `<div id ="smallCard${i}" class="smallCard">
     <div class="PokeImage">
     <img class="pokemon-img" src="${Front_img}">
     </div>
@@ -20,9 +21,20 @@ for (let i=0; i<resopnseAsJSON.results.length; i++){
      ${PokemonAsJSON['types'][0]['type']['name']}
      </div>
     </div>`;
-    
-    // document.getElementById('smallCard').innerHTML += `Type: ${Pokemon['types']['type'][1]}}`;
-    console.log(PokemonAsJSON);
 };
+giveTypeColor();
+}
 
+async function giveTypeColor(){
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=15&offset=0");
+    let resopnseAsJSON = await response.json(); 
+    
+
+    for (let i=0; i<resopnseAsJSON.results.length; i++){
+        let Pokemons = await resopnseAsJSON.results[i]['url'];
+    let Pokemon = await fetch(Pokemons);
+    let PokemonAsJSON= await Pokemon.json();
+    let type= PokemonAsJSON['types'][0]['type']['name'];
+        document.getElementById(`smallCard${i}`).classList.add(type);
+    };
 }

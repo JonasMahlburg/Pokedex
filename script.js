6e76de92b969314e3d2b6daee3aaf3666368647d
Jonas Mahlburg <jonas@LaptopvonJonas.fritz.box>
2024-06-05 16:15:55 +0200

    added loader and make code pretier

--- script.js
+++ script.js
@@ -2,25 +2,27 @@ let offset = 0;
 const limit = 15;
 let Pokedex = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 let allPokemon = [];
-// let searchable =`https://pokeapi.co/api/v2/pokemon?limit=151&offset=0`
 
 
 async function fetchDataJSON() {
   let response = await fetch(Pokedex);
   let responseAsJSON = await response.json();
-
+showLoader();
   for (let i = 0; i < responseAsJSON.results.length; i++) {
     let pokemonUrl = responseAsJSON.results[i].url;
     let pokemonResponse = await fetch(pokemonUrl);
     let pokemonAsJSON = await pokemonResponse.json();
     allPokemon.push(pokemonAsJSON);
     showAllPokemon(allPokemon.length - 1);
+    document.getElementById("loader").classList.add("d-none");
   }
 }
 
+function showLoader() {
+  document.getElementById("loader").classList.remove("d-none")
+}
+
 function showAllPokemon(i) {
-
-
   document.getElementById("content").innerHTML +=
     `<div id ="smallCard${i}" class="smallCard" onclick="showBigCard(${i}), hearScream(${i})">
                <div class="PokeImage">
@@ -39,13 +41,14 @@ function showAllPokemon(i) {
 async function fetchLoadedJSON() {
   let response = await fetch(Pokedex);
   let responseAsJSON = await response.json();
-
+  showLoader();
   for (let i = 0; i < responseAsJSON.results.length; i++) {
     let pokemonUrl = responseAsJSON.results[i].url;
     let pokemonResponse = await fetch(pokemonUrl);
     let pokemonAsJSON = await pokemonResponse.json();
     allPokemon.push(pokemonAsJSON);
     showLoadedPokemon(allPokemon.length - 1);
+    document.getElementById("loader").classList.add("d-none");
   }
 }
 
@@ -69,7 +72,8 @@ function showLoadedPokemon(i) {
 
 function filterPokemon() {
   let search = document.getElementById("search").value;
-  search = search.toLowerCase();
+  if(search.length>= 3){
+      search = search.toLowerCase();
   document.getElementById("content").innerHTML = "";
   for (let i = 0; i < allPokemon.length; i++) {
     if (allPokemon[i]["name"].toLowerCase().includes(search)) {
@@ -86,10 +90,10 @@ function filterPokemon() {
      </div>
     </div>`;
 
-
-    }; giveSearchColor(i);
+    giveSearchColor(i);
+    }; 
   }
-
+}
 }
 
 async function giveTypeColor() {
@@ -107,6 +111,7 @@ async function giveTypeColor() {
     }
   }
 }
+
 async function giveLoadedColor(k) {
   let response = await fetch(Pokedex);
   let resopnseAsJSON = await response.json();
@@ -125,16 +130,9 @@ async function giveLoadedColor(k) {
 }
 
 async function giveSearchColor(j) {
-  // let response = await fetch(Pokedex);
-  // let resopnseAsJSON = await response.json();
-
   if (blackscreen.classList.contains("d-none")) {
     for (let i = 0; i < allPokemon.length; i++) {
-      // let Pokemons = await resopnseAsJSON.results[j]["url"];
-      // let Pokemon = await fetch(Pokemons);
-      // let PokemonAsJSON = await Pokemon.json();
       let type = allPokemon[j]["types"][0]["type"]["name"];
-
       document.getElementById(`smallCard${j}`).classList.add(type);
     }
   }
@@ -165,7 +163,7 @@ function showBigCard(i) {
   <button class="tablinks" onclick="openCity(event, 'Moves')">Attacks</button>
   <button class="tablinks" onclick="openCity(event, 'Other')">Other Info</button>
 </div>
-     <div id="Stats" class="tabcontent" ></div>  
+     <div id="Stats" class="tabcontent" style="display: block" ></div>  
      <div id="Moves" class="tabcontent"></div>
      <div id="Other" class="tabcontent"></div>
      <div id="Entry" class="tabcontent"></div>
@@ -212,39 +210,30 @@ async function showOther(i) {
 
 function closeBigCard() {
   document.getElementById("blackscreen").classList.add("d-none");
-  // document.getElementById('bigCard').innerHTML="";
+  
 }
 
 async function giveColorBigCard(i) {
-
   let type = allPokemon[i]["types"][0]["type"]["name"];
-
   document.getElementById(`bigCard${i}`).classList.add(type);
 }
 
 async function hearScream(i) {
-
   let Audio_scream = new Audio(allPokemon[i]["cries"]["latest"]);
   Audio_scream.play();
 }
 
 function openCity(evt, cityName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
-  
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
@@ -259,11 +248,10 @@ async function loadMorePokemon() {
   Pokedex = `https://pokeapi.co/api/v2/pokemon?limit=${fetchLimit}&offset=${offset}`;
   await fetchLoadedJSON(Pokedex);
 
-
   if (allPokemon.length >= 151) {
     let loadMoreButton = document.getElementById('reloadBtn');
     loadMoreButton.disabled = true;
-    loadMoreButton.textContent = 'All Pokémon loaded';
+    alert('all Pokemon are loaded')
   }
 }
 
@@ -271,7 +259,6 @@ function nextPokeLeft(i) {
   document.getElementById("blackscreen").innerHTML = "";
  
   if (i > 0) {
-    
     showBigCard(i-1);
   } else {
     showBigCard(i);
@@ -284,7 +271,6 @@ function nextPokeRight(i) {
   document.getElementById("blackscreen").innerHTML = "";
   
   if (i >= allPokemon.length -1) {
-    
     showBigCard(i);
   } else {
     showBigCard(i+1);
